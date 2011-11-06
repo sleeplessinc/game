a8313f0ff0935c7744538ff4fb5a5f1f5039e691
Joe Hitchens <joe@sleepless.com>
2011-11-05 22:34:18 -0700

    -

--- x.js
+++ x.js
@@ -4,28 +4,28 @@ var util = require("util")
 
 log("\n\n");
 
+// a thing for holding stuff
 function Bag() {
 	this.put = function(o) {
-		this.push(o)
-		return this.length
+		var i = this.find(o)
+		if(i != -1) 
+			return false		// already present 
+		this.push(o)			// append to array
+		return true
 	}
 	this.get = function(o) {
-		if(typeof o == "number") {
-			return (o >= 0 && o < s.length) ? this[o] : null
-		}
 		var i = this.find(o)
-		if(i >= 0) {
-			this.splice(i,1)
-			return o
-		}
-		return null
+		if(i < 0)
+			return null			// not present
+		this.splice(i,1)		// remove from array
+		return o
 	}
 	this.find = function(o) {
 		for(var i = 0; i < this.length; i++) {
 			if(this[i] === o)
-				return i
+				return i		// found. return index
 		}
-		return -1
+		return -1				// not found
 	}
 }
 Bag.prototype = new Array()
@@ -34,166 +34,192 @@ Bag.prototype = new Array()
 function Thing(name) {
 	log("Thing("+name+")")
 
+	var t = new Object()
+
+	// set type by walking up the caller chain
+	// the function that creates things must be an unnamed function for this to work
 	var caller = arguments.callee.caller
 	while(caller.name) {
-		this.type = caller.name
+		t.type = caller.name
 		caller = caller.caller
-		log(this.type+"()...")
 	}
 
-	this.name = name || "thing"
+	t.name = name || "thing"
 
-	this.react = function(action, actor) {
+	t.react = function(action, actor) {
 		var a = action[0].toUpperCase() + action.substr(1)
 		var f = this["on"+a]
 		return f.call(this, actor)
 	}
 
-	this.onStudy = function(it) {
-		log(this.name+" appears to be an ordinary "+this.type)
+	t.onStudy = function(it) {
+		log(t.name+" appears to be an ordinary "+t.type)
 	}
 
+	return t
 }
 
 
 function Place(name, desc) {
-	Thing.apply(this, arguments)
+	var t = new Thing(name)
+	//Thing.apply(this, arguments)
 
-	this.desc = desc
-	this.things = new Bag()
+	t.desc = desc
+	t.things = new Bag()
 
-	this.onStudy = function(it) {
-		log("You are in a place called "+this.name)
-		log(this.desc)
-		log("You see "+this.things.length+" things:")
-		this.things.forEach(function(t, i) {
+	t.onStudy = function(it) {
+		log("You are in a place called "+t.name)
+		log(t.desc)
+		log("You see "+t.things.length+" things:")
+		t.things.forEach(function(t, i) {
 			log("    "+(i+1)+": A "+t.type+" named "+t.name)
 		})
 	}
 
+	return t
 }
 
 
 function Adventurer(name) {
-	Thing.apply(this, arguments)
+	var t = new Thing(name)
+	//Thing.apply(this, arguments)
 
-	this.pack = new Bag()
-	this.armor = null
-	this.weapon = null
+	t.place = null
+	t.pack = new Bag()
+	t.armor = null
+	t.weapon = null
 
-	this.study = function(it) {
+	t.study = function(it) {
 		log("study("+it.type+")")
-		it.react("study", this)
+		it.react("study", t)
 	}
 
-	this.read = function(it) {
+	t.read = function(it) {
 		log("read("+it.type+")")
-		it.react("read", this)
+		it.react("read", t)
 	}
 
-	this.attack = function(it) {
+	t.attack = function(it) {
 		log("attack("+it.type+")")
-		it.react("attack", this)
+		it.react("attack", t)
 	}
 
-	this.wear = function(it) {
+	t.wear = function(it) {
 		log("wear("+it.type+")")
-		it.react("wear", this)
+		it.react("wear", t)
 	}
 
-	this.wield = function(it) {
+	t.wield = function(it) {
 		log("wield("+it.type+")")
-		it.react("wield", this)
+		it.react("wield", t)
 	}
 
-	this.take = function(it) {
+	t.take = function(it) {
 		log("take("+it.type+")")
-		it.react("take", this)
-		this.showPack()
+		it.react("take", t)
+		t.showPack()
 	}
 
-	this.drop = function(it) {
+	t.drop = function(it) {
 		log("drop("+it.type+")")
-		it.react("drop", this)
-		this.showPack()
+		it.react("drop", t)
+		t.showPack()
 	}
 
-	this.showPack = function() {
-		log("Your pack contains "+this.pack.length+" thing(s):")
-		this.pack.forEach(function(o, i) {
+	t.showPack = function() {
+		log("Your pack contains "+t.pack.length+" thing(s):")
+		t.pack.forEach(function(o, i) {
 			log("    "+(i+1)+": A "+o.type+" named "+o.name)
 		})
 	}
 
+	return t
 }
 
 
 function Treasure(name) {
-	Thing.apply(this, arguments)
+	var t = new Thing(name)
+	//Thing.apply(this, arguments)
 
-	this.onTake = function(actor) {
-		actor.pack.put(this)
-		log(this.type+" taken")
+	t.onTake = function(actor) {
+		if(actor.place.things.get(t)) {
+			if(actor.pack.put(t))
+				log(t.type+" taken")
+		}
 	}
 
-	this.onDrop = function(actor) {
-		actor.pack.get(this)
-		log(this.type+" dropped")
+	t.onDrop = function(actor) {
+		if(actor.pack.get(t)) {
+			if(actor.place.things.put(t))
+				log(t.type+" dropped")
+		}
 	}
 
+	return t
 }
 
 
 function Scroll(name) {
-	Treasure.apply(this, arguments)
+	var t = new Treasure(name)
+	//Treasure.apply(this, arguments)
 
-	this.onRead = function(actor) {
+	t.onRead = function(actor) {
 		log("Nothing happens")
 	}
 
+	return t
 }
 
 
 function Armor(name) {
-	Treasure.apply(this, arguments)
+	var t = new Treasure(name)
+	//Treasure.apply(this, arguments)
 
-	this.onWear = function(actor) {
-		actor.armor = this
-		log("Wearing "+this.type)
+	t.onWear = function(actor) {
+		actor.armor = t
+		log("Wearing "+t.type)
 	}
 
+	return t
 }
 
 
 function Weapon(name) {
-	Treasure.apply(this, arguments)
+	var t = new Treasure(name)
+	//Treasure.apply(this, arguments)
 
-	this.onWield = function(actor) {
-		actor.weapon = this
-		log("Wielding "+this.type)
+	t.onWield = function(actor) {
+		actor.weapon = t
+		log("Wielding "+t.type)
 	}
 
+	return t
 }
 
 
 function Monster(name) {
-	Thing.apply(this, arguments)
+	var t = new Thing(name)
+	//Thing.apply(this, arguments)
 
-	this.onAttack = function(it) {
-		log(this.name+" laughs at your feeble aggression")
+	t.onAttack = function(it) {
+		log(t.name+" laughs at your feeble aggression")
 	}
 
+	return t
 }
 
 
-
-you = new Adventurer("Joe")
-place = new Place("Home", "A cabin in a meadow")
-monster = new Monster("Glarnmaggle")
-scroll = new Scroll("Aclirew")
-treasure = new Treasure("Gem")
-armor = new Armor("Leather armor")
-weapon = new Weapon("Rusty sword")
+log("---- creating stuff ---")
+foo = function() {
+	you = new Adventurer("Joe")
+	place = new Place("Home", "A cabin in a meadow")
+	you.place = place
+	monster = new Monster("Glarnmaggle")
+	scroll = new Scroll("Aclirew")
+	treasure = new Treasure("Gem")
+	armor = new Armor("Leather armor")
+	weapon = new Weapon("Rusty sword")
+} ; foo()
 
 place.things.put(monster)
 place.things.put(treasure)
@@ -201,6 +227,7 @@ place.things.put(scroll)
 place.things.put(armor)
 place.things.put(weapon)
 
+log("---- doing stuff ---")
 you.study(you)
 you.study(place)
 you.study(monster)
@@ -216,6 +243,7 @@ you.study(treasure)
 you.drop(treasure)
 
 you.take(armor)
+you.take(weapon)
 you.take(weapon)
 you.wield(weapon)
 you.wear(armor)
