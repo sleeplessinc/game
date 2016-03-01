228c51827ccb124638e6419ad2532cba6a5c182f
Joe Hitchens <joe@sleepless.com>
2016-02-29 23:37:53 -0800

    -

--- swords-2016.js
+++ swords-2016.js
@@ -14,10 +14,34 @@ game.world = {};
 game.world.places = [];
 game.world.places.push({
 	name: "Home",
-	desc: "Your home",
-	look: function(){
-		wrt("You see your home.\n")
-	}
+	enter: function(){
+		wrt("\nYou see your home.\n")
+	},
+	look: function() {
+		wrt( "\n"+
+			"Your home is located high in the mountains nestled in an alpine "+
+			"canyon and surrounded by majestic trees.  "+
+			"A meadow stretches out in front of your cabin and down the canyon.  "+
+			"You have always been at home in these woods and the "+
+			"birds are always singing in this peaceful place.  "+
+			"\n");
+	},
+	exits: {south: "trail"},
+});
+game.world.places.push({
+	name: "Trail",
+	enter: function() {
+		wrt("\nYou are on the river trail.\n");
+	},
+	look: function() {
+		wrt( "\n"+
+			"You are on what you call the 'river trail'.  "+
+			"There is good fishing in some spots here.  "+
+			"It runs along the north bank of the Hoopty river.  "+
+			"There is good fishing in some spots here.  "+
+			"\n");
+	},
+	exits: {north: "home"},
 });
 
 
@@ -25,7 +49,36 @@ here = game.world.places[0];
 
 
 look = function(args) {
+
 	here.look();
+
+	var a = []
+	for(var k in here.exits) {
+		a.push(k);
+	};
+	wrt("\nYou can go "+a.join(", ")+".\n");
+}
+
+go = function(args) {
+	var where = args[1];	
+	if(!where) {
+		wrt("Go where?\n");
+		return;
+	}
+	var there = here.exits[where];
+	if(!there) {
+		wrt("Can't go there.\n");
+		return;
+	}
+	for(var i = 0; i < game.world.places.length; i++) {
+		var place = game.world.places[i];
+		if(place.name.ucase() == there.ucase()) {
+			here = place;
+			here.enter([]);
+			return;
+		}
+	};
+	wrt("bad link: "+there+"\n");
 }
 
 
@@ -47,6 +100,10 @@ parse = function(line) {
 		wrt("What?\n");
 	}
 }
+
+
+here.enter([]);
+look([]);
 
 
 prompt = function() {
